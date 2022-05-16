03ca2d2d800954fd061bd70f271b65bca595c489
Thomas <Thomas@DESKTOP-BQE1IKF>
2022-05-16 15:59:31 +0200

    Added base structure & homepage

---
+++ PaletteLeuzoise/Controllers/HomepageController.cs
@@ -0,0 +1,33 @@
+﻿namespace PaletteLeuzoise.Site.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ViewEngines;
+    using Microsoft.Extensions.Logging;
+
+    using PaletteLeuzoise.Site.ViewModels;
+    
+    using Umbraco.Cms.Core.Web;
+    using Umbraco.Cms.Web.Common;
+    using Umbraco.Cms.Web.Common.Controllers;
+
+    public class HomepageController : RenderController
+    {
+        public HomepageController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) 
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        { }
+
+        public override IActionResult Index()
+        {
+            // TODO : Override Render controller to get content property directly
+            // TODO : Constants instead of magic strings 
+            var content = UmbracoContext.PublishedRequest.PublishedContent;
+
+            return CurrentTemplate(new HomepageViewModel()
+            {
+                Title = content.GetProperty("mainTitle").GetValue().ToString(),
+                DescriptionTitle = content.GetProperty("descriptionTitle").GetValue().ToString(),
+                DescriptionContent = content.GetProperty("descriptionContent").GetValue().ToString(),
+            });
+        }
+    }
+}
---
+++ PaletteLeuzoise/ViewModels/HomepageViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PaletteLeuzoise.Site.ViewModels
+{
+    using System.Web.Mvc;
+    public class HomepageViewModel
+    {
+        public string Title { get; set; }
+        public string DescriptionTitle { get; set; }
+        public string DescriptionContent { get; set; }
+    }
+}
